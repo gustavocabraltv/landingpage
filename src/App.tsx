1d03410524f214d7ef8072b8d480c8470dc8d557
Gustavo Cabral <studiogustavocabral@gmail.com>
2022-09-20 22:47:40 -0300

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-
-function App() {
-
+export default function App() {
   return (
-    <div className="App">
-      Landing Page
-    </div>
+    <h1 className="text-3xl font-bold ">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
