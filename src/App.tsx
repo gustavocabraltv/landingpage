8887062e8cf911569cb54f20d95d0ad20d7c38b5
Gustavo Cabral <studiogustavocabral@gmail.com>
2022-09-21 19:31:48 -0300

    Grid Tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold ">
-      Hello world!
-    </h1>
+   <section className="container mx-auto grid  gap-3 bg-slate-200 p-8 lg:grid-cols-3 sm:grid-cols-2 ">
+  <div className="flex h-52 w-full items-center justify-center bg-slate-100">asdasd</div>
+    <div className="flex h-52 w-full items-center justify-center bg-slate-100">asdasd</div>
+    <div className="flex h-52 w-full items-center justify-center bg-slate-100 ">asdasd</div>
+
+    <div className="flex h-52 w-full items-center justify-center bg-slate-100">asdasd</div>
+    <div className="flex h-52 w-full items-center justify-center bg-slate-100">asdasd</div>
+    <div className="flex h-52 w-full items-center justify-center bg-slate-100">asdasd</div>
+   </section>
   )
 }
